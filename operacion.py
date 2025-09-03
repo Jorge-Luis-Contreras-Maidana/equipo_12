b91995df4c5fa584539842150722aa5d96a8f3bb
Juandy <jsinanichavez5@gmail.com>
2025-09-03 11:17:25 -0400

    Codigo_python

---
+++ operacion.py
@@ -0,0 +1,12 @@
+# Operación simple
+numero1 = 5
+numero2 = 10
+suma = numero1 + numero2
+print("OPERACION SIMPLE")
+print("La suma de", numero1, "y", numero2, "es:", suma)
+print()
+print("INTEGRANTES")
+print("1 Juan Manuel Sinani Chavez - AUTOR")
+print("2 Heynar Freddy Mamani Quispe Mamani")
+print("3 Miguel Angel Salas Alanoca")
+print("4 Jorge Luis Contreras Maidana")
