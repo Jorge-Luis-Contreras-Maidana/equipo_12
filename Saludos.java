0212d9eaa101ec3cf4f29c792e38a09be97fd0b4
Miguel Angel Salas Alanoca <miguelangelsalasalanoca26@gmail.com>
2025-09-03 11:13:44 -0400

    mi archivo saludo java

---
+++ Saludos.java
@@ -0,0 +1,9 @@
+public class Saludo {
+ public static void main(String[] args) {
+ System.out.println("¡Hola, equipo!");
+ System.out.println("1 Salas Alanoca Miguel Angel");
+ System.out.println("2 Mamani Quispe Heynar Freddy");
+ System.out.println("3 Contreras Maidana Jorge Luis");
+ System.out.println("4 Siñani Chavez Juan Manuel");
+ }
+}
